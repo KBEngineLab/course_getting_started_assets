f77478aef0262fc42394219dcf3a1e55a1df9465
IvanDing <ivan.ding95@qq.com>
2026-03-10 03:39:21 +0800

    入门教程p7-NPC的创建

---
+++ common/data/d_npcs.py
@@ -0,0 +1,27 @@
+data = {
+	"xinshoucun": {
+		"npc1": {
+			"name": "新手接待员",
+			"position": (0, 0, 0),
+			"direction": (0, 0, 0),
+			"moveSpeed": 0,
+			"dialog": [
+				"你好玩家，我是新手接待员",
+				"有什么事情吗？"
+				"这是KBEngine Nex的入门教程",
+				"很高兴为你服务"
+			]
+		},
+		"npc2": {
+			"name": "鬼鬼祟祟的人",
+			"position": (0, 0, 0),
+			"direction": (0, 0, 0),
+			"moveSpeed": 5,
+			"dialog": [
+				"咳咳..",
+				"走开...."
+				"看不见我看不见我",
+			]
+		}
+	}
+}
